93ecff0b42a4a755111fa783364b208f3d64d833
Gisele <gisesonia@gmail.com>
2021-01-10 11:08:01 -0300

    Add Navbar

---
+++ front-web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Navbar from './Navbar/index';
+
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
