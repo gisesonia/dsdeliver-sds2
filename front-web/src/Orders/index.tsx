c0b151a9729b6f72bd5c65c565ee4881722f7bc4
Gisele <gisesonia@gmail.com>
2021-01-10 19:28:00 -0300

    Axios e formatação de moeda

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,10 +1,23 @@
 import StepsHeader from './StepHeader'
 import './styles.css'
+import { useEffect, useState } from 'react';
+import { Product } from './types';
+import { fetchProducts } from '../api';
+import ProductsList from './ProductsList';
 
 function Orders(){
+  const [products, setProducts] = useState<Product[]>([])
+
+  useEffect(() => {
+      fetchProducts()
+      .then(response => setProducts(response.data))
+      .catch(error => console.log(error))
+  }, []);
+
   return(
     <div className="orders-container">
      <StepsHeader />
+      <ProductsList products={products}/>
     </div>
   )
 }
