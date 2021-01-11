8faa1d5ff80fbbce4f7930705a02cf91caa08b27
Gisele <gisesonia@gmail.com>
2021-01-11 10:59:43 -0300

    atualizacao properties

--- backend/src/main/java/com/devsuperior/dsdeliver/controllers/OrderController.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/controllers/OrderController.java
@@ -15,7 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/orders")
 public class OrderController {
-
+//Injeção de dependencia
     @Autowired
     private OrderService service;
 
